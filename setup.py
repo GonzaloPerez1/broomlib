fb2cf556676778696620bf7e930fc0743f074567
GonzaloPerez1 <gonzalo.perez.diez@gmail.com>
2021-10-05 09:13:56 +0200

    Añadimos la url de descarga del archivo tar.gz

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'dsjun21',                   # Type in your name
   author_email = 'gonzalo.perez.diez@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/GonzaloPerez1/Libreria-python',   # Provide either the link to your github or to your website
-  #download_url = 'https://github.com/javapagar/lib_pythoneers/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/GonzaloPerez1/Libreria-python/archive/refs/tags/v_01.tar.gz',    # I explain this later on
   keywords = ['Machine Learning', 'Easy', 'Help','Visualization','Preprocessing','Cleaning'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
